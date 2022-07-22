ce5e036cf5a1c5d503e595d31c7ea5ec178ae060
darkso77 <darkso77@wp.pl>
2022-07-22 11:54:21 +0200

    Added file MapaDzienPracyRcp.cs

---
+++ MapaDzienPracyRcp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Program_1
+{
+    public class MapaDzienPracyRcp : ClassMap<DzienPracyRcp>
+    {
+        public MapaDzienPracyRcp()
+        {
+            Map(m => m.Kod_pracownika).Name(nameof(DzienPracyRcp.Kod_pracownika));
+            Map(m => m.data).Name(nameof(DzienPracyRcp.data));
+            Map(m => m.godzina_wejscia).Name(nameof(DzienPracyRcp.godzina_wejscia));
+            Map(m => m.godzina_wyjscia).Name(nameof(DzienPracyRcp.godzina_wyjscia));
+        }
+    }
+}
